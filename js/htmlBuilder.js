3cc167390fe5e37c24fba5d7c77e294ececbc5e0
Siarhei Fiadzisau <Siarhei_Fiadzisau@epam.com>
2016-12-06 19:56:50 +0300

    add news data entity

--- js/htmlBuilder.js
+++ js/htmlBuilder.js
@@ -1,22 +1,22 @@
 'use strict';
 
+let news = require('./news');
+
  module.exports.HtmlBuilder = class HtmlBuilder{
 	 
 	constructor(url, title, author){
-		this.url = url;
-		this.author = author;
-		this.title = title;
+		this.newsData = new news.news(url, title, author);
 	}
 	
 	createTitleHtml(){
 		let urlTempalate = document.createElement("div");
-			urlTempalate.innerHTML = `<a href='${this.url}'>${this.title}</a>`;
+			urlTempalate.innerHTML = `<a href='${this.newsData.url}'>${this.newsData.title}</a>`;
 			return urlTempalate.outerHTML;
 	}
 	
 	createAuthorHtml(){
 		let authorTemplate = document.createElement("div");
-			authorTemplate.innerHTML = `<i>${this.author}</i>`;
+			authorTemplate.innerHTML = `<i>${this.newsData.author}</i>`;
 			return authorTemplate.outerHTML;
 	}
 	
